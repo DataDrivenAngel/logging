226d3083e1f20eb425dd9ac3ffbdf2b6a2a386cf
angel <buildingwill@gmail.com>
2021-02-23 17:36:07 -0500

    update examples

--- code/1_basic_logging.py
+++ code/1_basic_logging.py
@@ -7,5 +7,4 @@ print(message)
 
 # enter the standard logging module
 import logging
-
 logging.warning(message)
--- code/4_log_rotation.py
+++ code/4_log_rotation.py
@@ -6,16 +6,20 @@ import logging.handlers
 logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
 
-#create a logging handler to handle the log rotation
+# Create a logging handler to handle the log rotation
 handler = logging.handlers.RotatingFileHandler(
               "rotating_log.log", maxBytes=2000, backupCount=5)
 
-handler.setFormatter('%(asctime)s %(levelname)s  %(message)s')
-#add handler to the logger
+# Format the messages
+handler.setFormatter(logging.Formatter('{asctime} {levelname} {message}', style= '{'))
+
+
+# Add handler to the logger
 logger.addHandler(handler)
 
 message = "I don't always log, but when I do, I use log rotation"
 
+# Log 100 entries
 for i in tqdm(range(100)):
     logger.info(message)
 
