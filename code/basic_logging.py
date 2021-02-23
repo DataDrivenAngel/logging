d7ece6a5f7b564cd0b39fcb2771aa89d063abdfe
angel <buildingwill@gmail.com>
2021-02-23 16:01:21 -0500

    update example

--- code/basic_logging.py
+++ code/basic_logging.py
@@ -2,10 +2,10 @@
 
 message = "I don't always debug, but when I do, I use print statements"
 
-#tis
+# "log" to console
 print(message)
 
+# enter the standard logging module
 import logging
 
-logging.basicConfig(format='%(asctime)s %(message)s',filename='basic_logging.log', level=logging.INFO)
-logging.info(message)
+logging.warning(message)
